5ee2e0671128f3c2b2fb88cd3d1cb1932bc7bdd1
MuhammadZainAmmad <zainammad05@gmail.com>
2023-06-26 08:50:01 -0700

    final version 1 of sending HTML tables to slack using pandas

--- task1/slack_task1_pandas.py
+++ task1/slack_task1_pandas.py
@@ -6,28 +6,16 @@ channel_id = "C05E96Q6PFB"
 slack_token = os.environ['slackAPIToken_task1']
 client = WebClient(token=slack_token)
 
-path = './sample_html.html'
+path = './modified_sampleHTML.html'
 with open(path, 'r') as file:
     html_content = file.read()
 
-dfs = pd.read_html(html_content)
+dfs = pd.read_html(html_content, header=None)
+print(dfs[0])
 markdown_table = dfs[0].to_markdown(index=False)
 
-# Send the message
-message = {
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": f"```{markdown_table}```"
-                        }
-                    }
-                ]
-            } # for sending mesage in code block
-response = client.chat_postMessage(channel=channel_id, text= 'html page', blocks=message['blocks'])
+response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
 
-# Check if the message was sent successfully
 if response['ok']:
     print('Message sent successfully!')
 else:
